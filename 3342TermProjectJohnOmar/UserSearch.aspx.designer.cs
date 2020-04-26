c413babf6ba7df833532b2723fd03ef7d45fa7d5
JohnRakowsky <tug52439@temple.edu>
2020-04-26 16:49:31 -0400

    Work on Search Page

--- 3342TermProjectJohnOmar/UserSearch.aspx.designer.cs
+++ 3342TermProjectJohnOmar/UserSearch.aspx.designer.cs
@@ -22,13 +22,13 @@ namespace _3342TermProjectJohnOmar {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// navbarDropdownMenuLink control.
+        /// navbarAccount control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor navbarDropdownMenuLink;
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor navbarAccount;
         
         /// <summary>
         /// manageAccount control.
